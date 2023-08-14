76a127fb16bf88bf10e028506d02c8adfac079cb
knightbane186 <denverlopes93@gmail.com>
2023-08-14 14:16:49 +1000

    log14aug

---
+++ MessApp/api/index.js
@@ -0,0 +1,40 @@
+// below will use to build a web application framework
+const express = require("express");
+// below is used to access the middle body?
+const bodyParser = require("body-parser");
+// mongoose is to access the middleware?
+const mongoose = require("mongoose");
+// below is used to access the authentication part of the app
+const passport = require("passport");
+// below is for user name password authentication of the app
+const localStrategy = require("passport-local").Strategy
+
+// we are initializinfg here 
+
+const app = express();
+const port = 8000;
+const cors = require("cors");
+app.use(cors());
+
+// now we configure the body parser and otehr necessary middleware 
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+const jwt = require("jsonwebtoken")
+
+mongoose.connect(
+    "mongodb+srv://denverlopes93:denver1993@cluster0.xxwntq7.mongodb.net/",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(()=> {
+    console.log("Connected to Mongo Db");
+    }).catch((err) => {
+        console.log("Error connecting to mongo Db", err)
+    });
+
+    app.listen(port,() => {
+console.log("Server running on port 8000");
+    });
