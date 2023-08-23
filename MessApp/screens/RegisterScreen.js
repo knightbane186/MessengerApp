b9a6069bbbc90a1a7b156631263244c2e68f10de
knightbane186 <denverlopes93@gmail.com>
2023-08-23 21:38:00 +1000

    23aug

--- MessApp/screens/RegisterScreen.js
+++ MessApp/screens/RegisterScreen.js
@@ -27,7 +27,7 @@ const RegisterScreen = () => {
 
     // send a POST  request to the backend API to register the user
     axios
-      .post("http://localhost:8000/register", user)
+      .post("http://localhost:192.168.1.101/register", user)
       .then((response) => {
         console.log(response);
         Alert.alert(
